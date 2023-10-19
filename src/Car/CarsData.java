d68b2cc228c58dd9d343804c3bd6fdf92b3b67e1
abdukar1m0vA <101871619+abdukar1m0vA@users.noreply.github.com>
2023-10-20 00:52:37 +0500

    1-todo

---
+++ src/Car/CarsData.java
@@ -0,0 +1,9 @@
+package Car;
+
+public class CarsData {
+    public static final String TANISHTIRUV = "Online mashina bozorimizga xush kelibsiz!";
+
+    public static final String TANISHTIRUV_RU = "Добро пожаловать на наш интернет-авторынок!";
+
+
+}

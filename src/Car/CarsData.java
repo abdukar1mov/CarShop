dc213fcf597b1f99b28ff6f152bee9f80f337abb
abdukar1m0vA <101871619+abdukar1m0vA@users.noreply.github.com>
2023-10-24 11:05:53 +0500

    Doing Arrays

--- src/Car/CarsData.java
+++ src/Car/CarsData.java
@@ -1,33 +1,13 @@
 package Car;
-import java.util.Scanner;
 
 public class CarsData {
+    //u
     public static final String TANISHTIRUV = "Online mashina bozorimizga xush kelibsiz!";
+    public static final String EMAIL = "Email va parolingizni kiriting";
 
+
+    //e
     public static final String TANISHTIRUV_RU = "Добро  на наш интернет-авторынок!";
+    public static final String EMAIL_EN = "Введите емайл и пароль";
 
-    public static class Main{
-        static String email = "123456@gmail.com";
-
-        static int password = 12345;
-
-        static char language = 'u';
-        public static void main(String args[]){
-            while(true){
-                System.out.println("""
-                        Выберите Язык
-                        1.UZB
-                        2.RUS
-                        """);
-                int chooseLan = (new Scanner(System.in).nextInt());
-                if (chooseLan == 1){
-                    language = 'u';
-                }else if (chooseLan == 2 ) {
-                    language = 'r';
-                }else System.err.println("404");
-
-                System.out.println(language == 'u' ? TANISHTIRUV : TANISHTIRUV_RU);
-            }
-        }
-    }
 }
--- src/Car/Exp.java
+++
@@ -1,4 +0,0 @@
-package Car;
-
-public class Exp {
-}

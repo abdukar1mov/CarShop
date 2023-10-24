496ebd9c76418a1a723339a3562e4468df8cbd53
abdukar1m0vA <101871619+abdukar1m0vA@users.noreply.github.com>
2023-10-24 11:13:52 +0500

    ARRAYS TYPES

--- src/Car/Cars.java
+++ src/Car/Cars.java
@@ -1,7 +1,17 @@
 package Car;
 
 public class Cars {
+    public static final String CARS_CATEGORY = """
+            
+            """;
     public static final String CARS_MODEL = """
             
             """;
+    public static final String CARS_CHARACTERS = """
+            
+            """;
+    public static final String CARS_PRICE = """
+            
+            """;
+
 }

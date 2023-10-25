16127be7a22aca4130d2a3ab803d732f20ff4a2d
abdukar1m0vA <101871619+abdukar1m0vA@users.noreply.github.com>
2023-10-25 08:59:38 +0500

    Login and register

--- src/Car/CarsData.java
+++ src/Car/CarsData.java
@@ -3,11 +3,28 @@ package Car;
 public class CarsData {
     //u
     public static final String TANISHTIRUV = "Online mashina bozorimizga xush kelibsiz!";
-    public static final String EMAIL = "Email va parolingizni kiriting";
-
+    public static final String EMAIL_UZ = "Emailingizni kiriting ";
+    public static final String PASSWORD_UZ = "Parolingizni kiriting";
 
     //e
-    public static final String TANISHTIRUV_RU = "Добро  на наш интернет-авторынок!";
-    public static final String EMAIL_EN = "Введите емайл и пароль";
+    public static final String TANISHTIRUV_EN = "Welcome to our Auto Salon!";
+    public static final String EMAIL_EN = "Enter the email";
+    public static final String PASSWORD_EN = "Enter the password";
 
+    //Main menu
+
+    public static final String MAIN_MENU_EN = """
+            1. Category
+            2. Settings
+            3. About Us
+            4. Change Language
+            5. Exit
+            """;
+    public static final String MAIN_MENU_UZ = """
+            1. Turkumi
+            2. Sozlamalar
+            3. Biz haqimizda
+            4. Til ozgartirish
+            5. Chiqish
+            """;
 }
--- src/Car/Main.java
+++ src/Car/Main.java
@@ -4,34 +4,97 @@ import java.util.Scanner;
 
 public class Main {
     static String email = "123456@gmail.com";
-
     static int password = 12345;
 
-    static char language = 'u';
+    static char language = 'e';
     public static void main(String args[]){
+        int user = (new Scanner(System.in).nextInt());
+        switch (user){
+            case 1:
+                registration();
+                break;
+            case 2:
+                logination();
+                break;
+            default:
+        }
         while(true){
             System.out.println("""
-                        Выберите Язык
+                        
                         1.UZB
-                        2.RUS
+                        2.EN
                         """);
             int chooseLan = (new Scanner(System.in).nextInt());
             if (chooseLan == 1){
                 language = 'u';
             }else if (chooseLan == 2 ) {
-                language = 'r';
+                language = 'e';
             }else System.err.println("404");
 
-            System.out.println(language == 'u' ? TANISHTIRUV : TANISHTIRUV_RU);
-            System.out.println(language == 'u' ? EMAIL : EMAIL_EN);
+            System.out.println(language == 'u' ? TANISHTIRUV : TANISHTIRUV_EN);
+            System.out.println(language == 'u' ? EMAIL_UZ : EMAIL_EN);
+            String email  = (new Scanner(System.in).next());
+
             int correctPassword = (new Scanner(System.in).nextInt());
             if (correctPassword == password){
-                int menu = (new Scanner(System.in).nextInt());
-                switch (menu){
-                    case 1:
-
+                boolean exit = true;
+                while(exit){
+                    categoryMenu();
+                    int menu = (new Scanner(System.in).nextInt());
+                    switch (menu){
+                        case 1:
+                            category();
+                            break;
+                        case 2:
+                            settings();
+                            break;
+                        case 3:
+                            aboutUs();
+                            break;
+                        case 4:
+                            changeLanguage();
+                            break;
+                        case 5:
+                            exit = false;
+                            break;
+                        default:
+                    }
                 }
-            };
+            }else {
+                System.out.println("Incorrect password");
+            }
         }
+    }
+
+    private static void logination() {
+        System.out.println(language == 'e' ? EMAIL_EN : EMAIL_UZ);
+        String email = (new Scanner(System.in).nextLine());
+        
+        System.out.println(language == 'e' ? PASSWORD_EN : PASSWORD_UZ);
+        String password = (new Scanner(System.in).nextLine());
+
+    }
+
+    private static void registration() {
+    }
+
+    private static void changeLanguage() {
+        System.out.println("Change lan");
+    }
+
+    private static void aboutUs() {
+        System.out.println("About us");
+    }
+
+    private static void settings() {
+        System.out.println("Setting");
+    }
+
+    private static void category() {
+        System.out.println("Categor");
+    }
+
+    private static void categoryMenu() {
+        System.out.println(language == 'e' ? MAIN_MENU_EN : MAIN_MENU_UZ);
     }
 }
